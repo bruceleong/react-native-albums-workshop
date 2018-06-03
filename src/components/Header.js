b1c4599fcc959333e53c99037067b42925c6e59c
Bruce Leong <brucewleong@gmail.com>
2018-06-03 17:18:23 -0400

    feature: completed styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,32 @@
-// import libraries for making a component
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { Text, View } from 'react-native';
 
-//make a component
 const Header = (props) => {
-  const { textStyle, viewStyles } = styles
+  const { textStyle, viewStyle } = styles;
+
   return (
-    <View style={viewStyles}>
-    <Text style={textStyle}>Albums</Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = {
-  viewStyles: {
+  viewStyle: {
     backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
-    height: 70,
-    paddingTop: 30,
+    height: 60,
+    paddingTop: 15,
     shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
+    shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     elevation: 2,
     position: 'relative'
   },
   textStyle: {
-    fontSize: 20,
+    fontSize: 20
   }
-}
+};
 
 export default Header;
